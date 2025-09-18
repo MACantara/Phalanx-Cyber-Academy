952ccdc9abd2724dd405ce9056ba448efdec1610
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-09-19 06:55:02 +0800

    Add CSV email loading function import to EmailCompletionTracker

--- app/static/js/simulated-pc/desktop-components/desktop-applications/email-functions/email-completion-tracker.js
+++ app/static/js/simulated-pc/desktop-components/desktop-applications/email-functions/email-completion-tracker.js
@@ -1,4 +1,4 @@
-import { ALL_EMAILS } from '../../../levels/level-two/emails/email-registry.js';
+import { ALL_EMAILS, loadEmailsFromCSV } from '../../../levels/level-two/emails/email-registry.js';
 import { EmailSessionSummary } from './email-session-summary.js';
 
 export class EmailCompletionTracker {
