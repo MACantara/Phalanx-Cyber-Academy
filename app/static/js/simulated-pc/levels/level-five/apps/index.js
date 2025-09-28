09582e2acb708a2de55ef6cc6cfe50185fe70e74
MACantara <cantara.michaelangelo@gmail.com>
2025-09-28 23:00:00 +0800

    Remove forensic applications from Level 5 Apps definition

--- app/static/js/simulated-pc/levels/level-five/apps/index.js
+++ app/static/js/simulated-pc/levels/level-five/apps/index.js
@@ -4,27 +4,7 @@
  */
 
 export const Level5Apps = {
-    // Forensic image analyzer
-    forensicImageAnalyzer: {
-        enabled: true,
-        supportedFormats: ['raw', 'e01', 'dd']
-    },
-    
-    // Timeline analyzer
-    timelineAnalyzer: {
-        enabled: true,
-        features: [
-            'file_timeline',
-            'network_timeline',
-            'user_activity'
-        ]
-    },
-    
-    // Evidence chain tracker
-    evidenceChainTracker: {
-        enabled: true,
-        hashAlgorithms: ['md5', 'sha1', 'sha256']
-    }
+    // Level 5 apps will be defined here
 };
 
 export default Level5Apps;
