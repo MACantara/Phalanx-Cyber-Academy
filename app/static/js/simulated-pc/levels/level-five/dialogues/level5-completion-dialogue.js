9d6434c11c7c363db7681981fcee1b2244119845
MACantara <cantara.michaelangelo@gmail.com>
2025-09-30 05:44:02 +0800

    Fix import path for BaseDialogue in Level 5 Completion Dialogue

--- app/static/js/simulated-pc/levels/level-five/dialogues/level5-completion-dialogue.js
+++ app/static/js/simulated-pc/levels/level-five/dialogues/level5-completion-dialogue.js
@@ -1,4 +1,4 @@
-import { BaseDialogue } from '../../dialogues/base-dialogue.js';
+import { BaseDialogue } from '../../../dialogues/base-dialogue.js';
 
 /**
  * Level 5 Completion Dialogue - Hunt for The Null Investigation Complete
