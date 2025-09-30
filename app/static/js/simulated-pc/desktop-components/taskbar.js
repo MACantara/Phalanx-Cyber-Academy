00946297214ba58df36333d60df2a61453a62fff
MACantara <cantara.michaelangelo@gmail.com>
2025-09-30 18:18:03 +0800

    Update taskbar button label to 'Quit Simulation'

--- app/static/js/simulated-pc/desktop-components/taskbar.js
+++ app/static/js/simulated-pc/desktop-components/taskbar.js
@@ -17,7 +17,7 @@ export class Taskbar {
         this.taskbarElement.className = 'absolute bottom-0 left-0 w-full h-12 bg-gray-800 border-t border-gray-600 flex items-center px-2.5 shadow-lg';
         this.taskbarElement.innerHTML = `
             <button class="bg-gray-700 border border-gray-600 text-white px-4 py-2 text-xs font-mono hover:bg-green-400 hover:text-black transition-all duration-200 hover:shadow-lg cursor-pointer rounded" id="start-btn">
-                <i class="bi bi-grid-3x3-gap mr-1"></i> Start
+                <i class="bi bi-grid-3x3-gap mr-1"></i> Quit Simulation
             </button>
             <div class="flex-1 flex items-center space-x-2.5 ml-5" id="taskbar-items"></div>
             <div class="flex items-center space-x-4">
