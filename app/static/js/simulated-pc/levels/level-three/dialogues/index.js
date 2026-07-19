8ba8a5fe1cda3a411f80a734804818c48bb6f724
Michael Angelo R. Cantara <cantara.michaelangelo@gmail.com>
2026-07-19 22:27:11 +0800

    Remove legacy files

--- app/static/js/simulated-pc/levels/level-three/dialogues/index.js
+++
@@ -1,3 +0,0 @@
-// Export all level-three dialogues for easy importing
-export { LevelThreeCompletionDialogue } from './level-three-completion.js';
-export { Level3MalwareMayhemDialogue } from './level3-malware-mayhem.js';
--- app/static/js/simulated-pc/levels/level-two/dialogues/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Level 2: Shadow in the Inbox - Dialogue exports
- * Central export file for all Level 2 dialogue modules
- */
-
-export { Level2ShadowInboxDialogue } from './level2-shadow-inbox.js';
-export { EmailSecurityCompletionDialogue } from './email-security-completion-dialogue.js';
